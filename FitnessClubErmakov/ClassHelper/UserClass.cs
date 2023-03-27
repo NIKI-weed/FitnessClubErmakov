afacc857f21d3814cf96d3ace5e15bf8d74565de
MandFan <diyor160200@gmail.com>
2023-03-27 13:03:28 +0300

    Авторизация по паролю

---
+++ FitnessClubErmakov/ClassHelper/UserClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FitnessClubErmakov.DataBase;
+
+namespace FitnessClubErmakov.ClassHelper
+{
+    public class UserClass
+    {
+        public static DataBase.UserAuth AuthUser { get; set; }
+    }
+}
