a5e698af6f8c28fb1107dea0c3d88b29d7b596d9
NIKI-weed <dermakov020@gmail.com>
2023-04-11 23:21:36 +0300

    Кнопки1

---
+++ FitnessClubErmakov/Windows/Director/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using FitnessClubErmakov.DataBase;
+using FitnessClubErmakov.ClassHelper;
+
+namespace FitnessClubErmakov.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TxtNameUser.Text = "Пользователь: " + ClassHelper.UserClass.AuthUser.Login + " Роль: " + ClassHelper.UserClass.AuthUser.Role.RoleName;
+        }
+
+        private void BtnServiceListGo_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceListWindow serviceListWindow = new ServiceListWindow();
+            serviceListWindow.Show();
+            this.Close();
+        }
+
+        private void BtnClientListGo_Click(object sender, RoutedEventArgs e)
+        {
+            ClientListWindow clientListWindow = new ClientListWindow();
+            clientListWindow.Show();
+            this.Close();
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            AuthWindow authWindow = new AuthWindow();
+            this.Close();
+            authWindow.ShowDialog();
+        }
+    }
+}
